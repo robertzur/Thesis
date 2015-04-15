87cfb70d66a72fa098a99cf08c588e1ce4a7ec71
Robert Zur <robert.zur@live.com>
2015-04-15 16:41:38 +0200

    Contact router implementation.

--- routes/contact.js
+++ routes/contact.js
@@ -2,15 +2,32 @@ var express = require('express');
 var router = express.Router();
 var model = require('../model');
 
-var contact = new model.Contacts({name : 'Janusz'});
+var Contacts = model.Contacts;
 
-/* GET home page. */
+/* Test route */
 router.get('/', function(req, res, next) {
   res.end('Hi, Contact API is here');
 });
 
-router.get('/Janusz', function(req,res, next) {
-	res.end('Hi, it\'s '+contact.name);
-})
+/* Display list of contacts with pagination */
+router.get('/:pagenumber/:pagesize', function(req, res, next) {
+
+	//Retrieve parameters from URL
+	var pageNumber = (req.params.pagenumber != null && req.params.pagenumber > 0 ? req.params.pagenumber : 1);
+	var pageSize = (req.params.pagesize != null && req.params.pagesize > 0 ? req.params.pagesize : 1);
+
+	//Build query based upon provided parameters
+	var query = Contacts.find({}); //Get all
+	query.limit(pageSize);
+	query.skip((pageNumber - 1) * pageSize);
+
+	query.exec(function (err, docs) {
+		if(err) {
+			res.end('Error occured' + err.message);
+		} else {
+			res.json(docs);
+		}
+	});
+});
 
 module.exports = router;
