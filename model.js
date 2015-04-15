82236566dc3bc72d71541ee59182c2b1de798aaf
Robert Zur <robert.zur@live.com>
2015-04-15 15:11:38 +0200

    Initial data access module added.

---
+++ model.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var config =require('./config').dev;
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var Contact = new Schema({
+	 "name" : {type : String, index: true},
+	 "description" : String,
+	 "addressLine1" : String,
+	 "addressLine2" : String,
+	 "phoneNumber" : String,
+	 "cellNumber" : String,
+	 "email" : String,
+	 "skypeId" : String,
+	 "twitter" : String,
+	 "facebook" : String,
+	 "isContactGroup" : Boolean,
+	 "ownerId" : ObjectId,
+	 "parentId" : ObjectId,
+	 "tags" : [ ObjectId ] //array of ObjectId items
+});
+
+var User = new Schema ({
+	"username" : String,
+	"password" : String,
+	"salt" : String,
+	"email" : String,
+	"enabled" : { type: Boolean, default: true }
+});
+
+var Log = new Schema ({
+	"sourceIP" : String,
+	"sourceUserAgent" : String,
+	"eventType" : String,
+	"userName" : String,
+	"message" : String,
+	"timestamp" : { type: Date, default: Date.now }
+})
+
+var Tag = new Schema({
+	"name" : String
+})
+
+mongoose.connect(config.databaseUrl)
+
+var Contacts = mongoose.model('Contacts', Contact);
+var Users = mongoose.model('Users', User);
+var Logs = mongoose.model('Logs', Log);
+var Tags = mongoose.model('Tags', Tag);
+
+exports.Contacts = Contacts;
+exports.Users = Users;
+exports.Logs = Logs;
+exports.Tags = Tag;
